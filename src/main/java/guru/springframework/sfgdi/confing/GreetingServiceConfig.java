18ee644286a85e69242e32039aa1db4092b21039
Profudeinfo33 <dinu_petre26@yahoo.ro>
2021-11-04 06:47:49 +0200

    adding factory bean example, adding di config example, adding primary and profile examples

--- src/main/java/guru/springframework/sfgdi/confing/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/confing/GreetingServiceConfig.java
@@ -1,13 +1,58 @@
 package guru.springframework.sfgdi.confing;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
-import guru.springframework.sfgdi.services.SetterInjectedGreetService;
+import com.springframework.pets.PetService;
+import com.springframework.pets.PetServiceFactory;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Bean
+    PetServiceFactory petServiceFactory(){
+        return new PetServiceFactory();
+    }
+
+    @Profile({"dog", "default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("dog");
+    }
+
+    @Bean
+    @Profile("cat")
+    PetService catPetService(PetServiceFactory petServiceFactory){
+        return petServiceFactory.getPetService("cat");
+    }
+
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository(){
+        return new EnglishGreetingRepositoryImpl();
+    }
+
+    @Profile({"EN", "default"})
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository){
+        return new I18nEnglishGreetingService(englishGreetingRepository);
+    }
+
+    @Profile("ES")
+    @Bean
+    I18NSpanishService i18nService(){
+        return new I18NSpanishService();
+    }
+
+
+    @Primary
+    @Bean
+    PrimaryGreteingService primaryGreteingService(){
+        return new PrimaryGreteingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService(){
--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,13 +1,17 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
-@Profile({"EN", "default"})
-@Service("i18nService")
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+
 public class I18nEnglishGreetingService implements GreetingServices{
 
+   private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+         this.englishGreetingRepository = englishGreetingRepository;
+    }
+
     @Override
-    public String sayGreeting() {
+   public String sayGreeting() {
         return "Hello World - EN";
     }
 }
